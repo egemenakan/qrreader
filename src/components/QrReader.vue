855d08c10bbdd301beef7ba5565484b25cfeac28
egemenakan <egemenakan@gmail.com>
2023-01-22 22:40:38 +0300

    deneme

--- src/components/QrReader.vue
+++ src/components/QrReader.vue
@@ -3,9 +3,21 @@ import { ref } from "vue"
 import { StreamBarcodeReader } from "vue-barcode-reader"
 
 const decodedText = ref("")
+const scanner = ref(null)
 
 const onLoaded = () => {
   console.log("loaded")
+  setTimeout(() => {
+    scanner.value.codeReader.stream.getTracks().forEach(function (track) {
+      track.stop()
+    })
+  }, 2000)
+  setTimeout(() => {
+    scanner.value.codeReader.stream.getTracks().forEach(function (track) {
+      track.play()
+    })
+  }, 5000)
+  // document.querySelector("video").setAttribute("width", "100%")
 }
 
 const onDecode = (text) => {
@@ -15,16 +27,10 @@ const onDecode = (text) => {
 </script>
 
 <template>
-  <StreamBarcodeReader class="w-96" @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+  <StreamBarcodeReader ref="scanner" class="w-full" @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
   <h2>The decoded value in QR/barcode is</h2>
   <h2>{{ decodedText }}</h2>
+  adssd
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-.information {
-  margin-top: 100px;
-}
-</style>
+<style scoped></style>
